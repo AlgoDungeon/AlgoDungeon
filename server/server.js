72baf6b430c448d25d7b232f0fdde5f577aa0937
Jonah Stewart <jonahlstewart@gmail.com>
2021-01-11 17:14:12 -0500

    built out infastructure for redis db, connected to cloud server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,45 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
+// const path = require("path");
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io')(http);
 const app = express();
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// const uuid = require('node-uuid'); //used for creating games.
+const redis = require('redis');
+const axios = require('axios');
 
 const PORT = 8080 || process.env.PORT;
+
+// redis endpoint: redis-10330.c114.us-east-1-4.ec2.cloud.redislabs.com:10330
+// username: AlgoDungeonAdmin | password: AlgoDungeon22!
+
+const client = redis.createClient({
+  host: 'redis-10330.c114.us-east-1-4.ec2.cloud.redislabs.com',
+  port: 10330,
+  password: '5wxv6Fd3aXrNtA4lLg8rtV4E5QsHxuK5', // default user password
+});
+
+client.set('foo', 'bar', (err, reply) => {
+  if (err) throw err;
+  console.log(reply);
+
+  client.get('foo', (err, reply) => {
+    if (err) throw err;
+    console.log(reply);
+  });
+});
+
+socketio.on('connection', function (socket) {
+  console.log('user connected', socket.id);
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+const dispatcher = function (type, obj, toid) {
+  console.log(obj);
+  socketio.to(toid).emit(type, obj);
+};
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
